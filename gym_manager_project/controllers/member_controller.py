3178f71430dac065bd5f86f9b4c3dd216e712934
ljones-11 <116349877+ljones-11@users.noreply.github.com>
2022-12-04 16:46:26 +0000

    members list and show individual routes written, tested and rendered

--- gym_manager_project/controllers/member_controller.py
+++ gym_manager_project/controllers/member_controller.py
@@ -1,14 +1,23 @@
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, render_template
 from models.member import Member
 import repositories.member_repository as member_repository
 
 members_blueprint = Blueprint("members", __name__)
 
-#blueprint
 
 #GET list members '/members' repo.select_all, render list
 
+@members_blueprint.route("/members")
+def members():
+    members = member_repository.select_all()
+    return render_template("members/index.html", members = members)
+
 #GET show member '/members/<id>' repo.select(id), render 
+
+@members_blueprint.route("/members/<int:id>")
+def member(id):
+    member = member_repository.select(id)
+    return render_template("members/show.html", member=member)
 
 #DELETE POSTmethod, /members/<id>/delete, repo.delete redirect
 
