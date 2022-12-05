c02d60b464c679c36ebfdd7de5c64e14402752f9
ljones-11 <116349877+ljones-11@users.noreply.github.com>
2022-12-05 20:26:47 +0000

    only active workouts can be added to a booking

--- gym_manager_project/controllers/booking_controller.py
+++ gym_manager_project/controllers/booking_controller.py
@@ -24,13 +24,17 @@ def booking(id):
 @bookings_blueprint.route("/bookings/new")
 def new_booking():
     active_members = []
+    active_workouts = []
     members = member_repository.select_all()
     workouts = workout_repository.select_all()
     for member in members:
         if member.status == '1':
             active_members.append(member)
+    for workout in workouts:
+        if workout.status == '1':
+            active_workouts.append(workout)
 
-    return render_template("bookings/new.html", members=members, workouts=workouts, active_members = active_members)
+    return render_template("bookings/new.html", members=members, workouts=workouts, active_members = active_members, active_workouts = active_workouts)
 
 # CREATE POST method, redirect, request.form['data'], memebr and workout repo's.select new booking, bookingrepo.save 
 
