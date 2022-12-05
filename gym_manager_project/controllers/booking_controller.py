4d35447292825c793b6118c4dd0945cfe92638d1
ljones-11 <116349877+ljones-11@users.noreply.github.com>
2022-12-05 11:03:38 +0000

    booking delete function added

--- gym_manager_project/controllers/booking_controller.py
+++ gym_manager_project/controllers/booking_controller.py
@@ -36,3 +36,8 @@ def add_booking():
     return redirect("/bookings")
 
 # DELETE bookings/delete POST method, redirect, booking.repo.delete(id)
+
+@bookings_blueprint.route("/bookings/<int:id>/delete", methods = ['POST'])
+def delete_booking(id):
+    booking_repository.delete(id)
+    return redirect("/bookings")
